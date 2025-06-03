087814d2dbc071f1b4cdfd467826166d84d744be
ayoub <ayoubafdel@gmail.com>
2025-06-03 20:17:22 +0100

    fix: one prettier with .vscode settings

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,15 @@
-import routeConfig from "./routeConfig";
-import "./index.css";
+import routeConfig from './routeConfig';
+import './index.css';
 
-import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter(routeConfig);
 
-const domNode = document.getElementById("root");
+const domNode = document.getElementById('root');
 
 if (!domNode) {
-  throw new Error("Root element not found!");
+  throw new Error('Root element not found!');
 }
 
 const root = createRoot(domNode);
