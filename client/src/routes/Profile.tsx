0d1dab483813e811d9e7a535d19e48342e51d993
mouasit <mustaphaouassit60@gmail.com>
2025-06-30 20:52:04 +0100

    fix time

--- client/src/routes/Profile.tsx
+++ client/src/routes/Profile.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet';
+import Carousel from '../components/Carousel';
 
 export default function Profile() {
   return (
@@ -6,6 +7,9 @@ export default function Profile() {
       <Helmet>
         <title>Matcha - Profile</title>
       </Helmet>
+      <main className="mt-12 mb-22 lg:mb-5 lg:ml-57">
+        <Carousel />
+      </main>
     </>
   );
 }
