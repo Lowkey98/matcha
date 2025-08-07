d5b6add768e096ed1539662ecfe73122e8ab80c9
lowkey98 <ayoubafdel@gmail.com>
2025-08-07 23:26:31 +0100

    chore: log and extra line

--- client/src/routes/ProfileUser.tsx
+++ client/src/routes/ProfileUser.tsx
@@ -118,7 +118,6 @@ export default function ProfileUser() {
   }, [user]);
 
   if (targetUserInfo?.isBlock) return null;
-    console.log("targetUserInfo", targetUserInfo?.imagesUrls);
   if (targetUserInfo)
     return (
       <>
