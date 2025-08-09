e09a1909c3beab1ea637538d33101b9276d0c33d
mouasit <mustaphaouassit60@gmail.com>
2025-08-09 21:32:01 +0100

    fix chat version lg

--- client/src/routes/Messages.tsx
+++ client/src/routes/Messages.tsx
@@ -7,17 +7,19 @@ export default function Messages() {
       <Helmet>
         <title>Matcha - Messages</title>
       </Helmet>
-      <div className="absolute top-0 left-31 h-screen overflow-auto px-4 py-5">
-        <UserMessageCard />
-      </div>
-      <div className="absolute top-0 left-120 pt-5">
-        <Link to={'/profile'} className="cursor-pointer">
-          <span className="text-secondary font-bold">Username</span>
-          <div className="flex items-center gap-1">
-            <div className="size-2 rounded-full bg-[#71D191]"></div>
-            <span className="text-grayDark text-sm font-light">Online</span>
-          </div>
-        </Link>
+      <div className="absolute top-0 left-31 flex">
+        <div className="border-grayDark-100 h-screen overflow-auto border-r px-4 py-5">
+          <UserMessageCard />
+        </div>
+        <div className="pt-5 pl-4">
+          <Link to={'/profile'} className="cursor-pointer">
+            <span className="text-secondary font-bold">Username</span>
+            <div className="flex items-center gap-1">
+              <div className="size-2 rounded-full bg-[#71D191]"></div>
+              <span className="text-grayDark text-sm font-light">Online</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
@@ -27,7 +29,7 @@ function UserMessageCard() {
   return (
     <button
       type="button"
-      className="border-grayDark-100 last:rounded- flex cursor-pointer items-center gap-2 border-b bg-gray-50 p-4 text-left first:rounded-t-lg last:rounded-b-lg last:border-none hover:bg-gray-50"
+      className="border-grayDark-100 flex cursor-pointer items-center gap-2 border-b bg-gray-50 p-4 text-left first:rounded-t-lg last:rounded-b-lg last:border-none hover:bg-gray-50"
     >
       <img
         src="/profile-slides-images/slide-1.jpg"
@@ -36,7 +38,7 @@ function UserMessageCard() {
       />
       <div className="flex flex-col">
         <span className="text-secondary text-sm font-semibold">Username</span>
-        <div className="text-grayDark w-58 overflow-hidden text-xs overflow-ellipsis whitespace-nowrap">
+        <div className="text-grayDark w-[20vw] overflow-hidden text-xs overflow-ellipsis whitespace-nowrap xl:w-58">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
           dolorum dicta odit obcaecati amet. Eius quia laudantium error deserunt
           quam repellendus recusandae dolor. Quas dolore accusantium
