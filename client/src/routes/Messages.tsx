2ff973d08756e98289fcde5f93984073f535f6fd
mouasit <mustaphaouassit60@gmail.com>
2025-08-10 16:08:14 +0100

    feat: add chat box

--- client/src/routes/Messages.tsx
+++ client/src/routes/Messages.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
-import { ArrowLeftIcon } from '../components/Icons';
+import { ArrowLeftIcon, SendMessageIcon } from '../components/Icons';
 
 export default function Messages() {
   return (
@@ -17,20 +17,47 @@ export default function Messages() {
 
 function ChatDesktop() {
   return (
-    <div className="absolute top-0 left-31 hidden lg:flex">
-      <div className="border-grayDark-100 h-screen overflow-auto border-r px-4 py-5">
-        <UserMessageCardDesktop />
+    <>
+      <div className="absolute top-0 left-31 hidden lg:flex">
+        <div className="border-grayDark-100 h-screen overflow-auto border-r px-4 py-5">
+          <UserMessageCardDesktop />
+        </div>
+        <div className="flex flex-col items-start pt-5 pl-4">
+          <Link to={'/profile'} className="flex items-center gap-2">
+            <img
+              src="/profile-slides-images/slide-1.jpg"
+              alt="user"
+              className="size-13 shrink-0 rounded-full object-cover"
+            />
+            <div>
+              <span className="text-secondary font-bold">Username</span>
+              <div className="flex items-center gap-1">
+                <div className="size-2 rounded-full bg-[#71D191]" />
+                <span className="text-grayDark text-sm font-light">Online</span>
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
-      <div className="pt-5 pl-4">
-        <Link to={'/profile'} className="cursor-pointer">
-          <span className="text-secondary font-bold">Username</span>
-          <div className="flex items-center gap-1">
-            <div className="size-2 rounded-full bg-[#71D191]"></div>
-            <span className="text-grayDark text-sm font-light">Online</span>
-          </div>
-        </Link>
+      <div className="ml-107 hidden flex-1 flex-col overflow-hidden pb-5 pl-4 lg:flex xl:ml-115 [:has(&)]:flex [:has(&)]:h-full [:has(&)]:w-full [:has(&)]:flex-1 [:has(&)]:flex-col">
+        <div className="mt-5 flex-1 space-y-2 overflow-auto">
+          <TargetBoxMessage />
+          <ActorBoxMessage />
+        </div>
+        <div className="pt-5">
+          <form className="bg-grayLight flex items-center gap-4 rounded-lg px-4">
+            <input
+              type="text"
+              placeholder="Send a message"
+              className="text-secondary w-full py-4 text-sm font-normal outline-0 placeholder:text-sm placeholder:text-[#B1B1B1]"
+            />
+            <button type="button">
+              <SendMessageIcon className="fill-primary size-5" />
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -39,19 +66,6 @@ function ChatMobile() {
   return (
     <>
       <main className="mt-8 mb-24 lg:hidden">
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
-        <UserMessageCardMobile setShowChatBox={setShowChatBox} />
         <UserMessageCardMobile setShowChatBox={setShowChatBox} />
       </main>
       {showChatBox ? <ChatBoxMobile setShowChatBox={setShowChatBox} /> : null}
@@ -72,7 +86,7 @@ function UserMessageCardDesktop() {
       />
       <div className="flex flex-col">
         <span className="text-secondary text-sm font-semibold">Username</span>
-        <div className="text-grayDark w-[20vw] overflow-hidden text-xs overflow-ellipsis whitespace-nowrap xl:w-58">
+        <div className="text-grayDark w-50 overflow-hidden text-xs overflow-ellipsis whitespace-nowrap xl:w-58">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
           dolorum dicta odit obcaecati amet. Eius quia laudantium error deserunt
           quam repellendus recusandae dolor. Quas dolore accusantium
@@ -124,7 +138,7 @@ function ChatBoxMobile({
     setShowChatBox(false);
   }
   return (
-    <div className="fixed inset-0 z-10 bg-white px-5 pt-5 lg:hidden [:has(&)]:overflow-hidden [:has(&)]:lg:overflow-visible">
+    <div className="fixed inset-0 z-10 flex flex-col bg-white px-5 pt-5 lg:hidden [:has(&)]:overflow-hidden [:has(&)]:lg:overflow-visible">
       <div className="flex items-center gap-4">
         <button
           type="button"
@@ -149,6 +163,61 @@ function ChatBoxMobile({
             </div>
           </div>
         </Link>
+      </div>
+      <div className="flex flex-1 flex-col overflow-hidden pb-5">
+        <div className="mt-5 flex-1 space-y-2 overflow-auto">
+          <TargetBoxMessage />
+          <ActorBoxMessage />
+          <TargetBoxMessage />
+          <ActorBoxMessage />
+          <TargetBoxMessage />
+          <ActorBoxMessage />
+          <TargetBoxMessage />
+          <ActorBoxMessage />
+          <TargetBoxMessage />
+          <ActorBoxMessage />
+        </div>
+        <div className="pt-5">
+          <form className="bg-grayLight flex items-center gap-4 rounded-lg px-4">
+            <input
+              type="text"
+              placeholder="Send a message"
+              className="text-secondary w-full py-4 text-sm font-normal outline-0 placeholder:text-sm placeholder:text-[#B1B1B1]"
+            />
+            <button type="button">
+              <SendMessageIcon className="fill-primary size-5" />
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TargetBoxMessage() {
+  return (
+    <div className="flex w-fit max-w-120 flex-col items-start text-sm">
+      <div className="bg-primary rounded-tr-2xl rounded-b-2xl p-4 break-all text-white">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry ☺️
+      </div>
+      <div className="mt-1 flex w-full justify-end">
+        <span className="text-grayDark font-light">00:25</span>
+      </div>
+    </div>
+  );
+}
+function ActorBoxMessage() {
+  return (
+    <div className="flex justify-end">
+      <div className="flex w-fit max-w-120 flex-col items-start text-sm">
+        <div className="text-secondary border-grayDark-100 rounded-tl-2xl rounded-b-2xl border-2 bg-white p-4 break-all">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry ☺️
+        </div>
+        <div className="mt-1 flex w-full justify-end">
+          <span className="text-grayDark font-light">00:25</span>
+        </div>
       </div>
     </div>
   );
