09a69165a6215ee1a3bbcbb7ad0124a32fa91c5c
mouasit <mustaphaouassit60@gmail.com>
2025-08-16 11:45:45 +0100

    remove log

--- server/index.ts
+++ server/index.ts
@@ -647,7 +647,6 @@ app.get('/api/verify', async (req, res) => {
 app.get('/api/getAllUsers', async (req, res) => {
   const token = req.headers.authorization?.split(' ')[1];
 
-  console.log('token', token);
   if (!token) {
     res.status(401).json({ error: 'Unauthorized' });
     return;
